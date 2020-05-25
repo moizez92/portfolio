9e6d3b73d16ec80e2dc514855db0c254d287acec
moizez92 <64280864+moizez92@users.noreply.github.com>
2020-05-25 19:31:58 +0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	$('.slider').slick({
+		arrows:true,
+		dots:true,
+		adaptiveHeight:true,
+		speed:500,
+		infinite:false,
+		autoplay:false,
+		autoplaySpeed:3000,
+		pauseOnFocus:true,
+		pauseOnHover:true,
+		pauseOnDotsHover:true,
+		touchThreshold:10,
+		
+	});
+});
+
---
+++ script/script_header.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function() {
+	$('.header_burger').click(function(event) {
+		$('.header_burger,.header_menu').toggleClass('active')
+	});
+});
+
+
+
+
+
